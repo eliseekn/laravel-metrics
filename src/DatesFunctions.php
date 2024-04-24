9d1d86de6fd6f3010f61d78dfee5b5e5911321fc
eliseekn <eliseekn@gmail.com>
2024-04-24 11:53:59 +0000

    Fix missing data labels auto discovery

--- src/DatesFunctions.php
+++ src/DatesFunctions.php
@@ -206,7 +206,7 @@ trait DatesFunctions
         $result = [];
 
         $labelColumn = explode('.', $this->labelColumn)[1];
-        $missingDataLabels = DB::table($this->builder->from)->get()->pluck($labelColumn)->toArray();
+        $missingDataLabels = DB::table($this->table)->get()->pluck($labelColumn)->toArray();
 
         foreach ($missingDataLabels as $label) {
             $result[$label] = $this->missingDataValue;
