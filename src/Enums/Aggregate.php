d2f4397611541063347382d577077e25bd19fc38
eliseekn <eliseekn@gmail.com>
2026-06-09 07:01:04 +0000

    chore

--- src/Enums/Aggregate.php
+++ src/Enums/Aggregate.php
@@ -7,9 +7,13 @@ namespace Eliseekn\LaravelMetrics\Enums;
 enum Aggregate: string
 {
     case COUNT = 'count';
+
     case AVERAGE = 'avg';
+
     case SUM = 'sum';
+
     case MAX = 'max';
+
     case MIN = 'min';
 
     public static function values(): array
--- src/Enums/Period.php
+++ src/Enums/Period.php
@@ -7,9 +7,13 @@ namespace Eliseekn\LaravelMetrics\Enums;
 enum Period: string
 {
     case TODAY = 'today';
+
     case DAY = 'day';
+
     case WEEK = 'week';
+
     case MONTH = 'month';
+
     case YEAR = 'year';
 
     public static function values(): array
